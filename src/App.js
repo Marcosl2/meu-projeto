b681e934db38c9bf0d855ffa33d9c949250a2f04
Marcosl2 <lamed-4@hotmail.com>
2023-04-29 14:35:01 -0300

    criado o components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+const nome = 'Marcos Lopes'
+const newName = nome.toUpperCase()
+const url = 'https://placeholder.com/150'
+
   return (
     <div className="App">
-      <h2>Alterando meu jsx</h2>
+      <h2>Alterando o JSX</h2>
+      <p> Olá, {newName}</p>
+      <img src={url} alt="Minha Imagem"/>
+      <HelloWorld/>
+
     </div>
   );
 }
